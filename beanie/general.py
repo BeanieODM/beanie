8ee7aaf559e2048e9b89e3ddb9dca042fbf34cfe
Mohamed Nesredin <m.n.kaizen@gmail.com>
2021-03-18 19:11:36 +0300

    add support for string base import for document_models

--- beanie/general.py
+++ beanie/general.py
@@ -1,12 +1,27 @@
-from typing import List, Type
+import importlib
+
+from typing import List, Type, Union
 
 from motor.motor_asyncio import AsyncIOMotorDatabase
 
 from beanie import Document
 
 
+def get_model(dot_path: str) -> Type[Document]:
+    try:
+        module_name, class_name = dot_path.rsplit(".", 1)
+        return getattr(importlib.import_module(module_name), class_name)
+
+    except ValueError:
+        raise ValueError(f"'{dot_path}' doesn't have '.' path, eg. path.to.your.model.class")
+
+    except AttributeError:
+        raise AttributeError(f"module '{module_name}' has no class called '{class_name}'")
+
+
 async def init_beanie(
-    database: AsyncIOMotorDatabase, document_models: List[Type[Document]]
+        database: AsyncIOMotorDatabase, document_models: List[Union[Type[Document], str]]
 ):
-    for model in document_models:
+    for model_path in document_models:
+        model = get_model(model_path) if isinstance(model_path, str) else model_path
         await model.init_collection(database)
