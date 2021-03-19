7ffb768b1cd99944f90347dd0862d38e6fd92562
Roman <roman-right@protonmail.com>
2021-03-19 20:58:42 +0100

    Documentation link in the deprecation warning

--- beanie/deprecated.py
+++ beanie/deprecated.py
@@ -7,8 +7,9 @@ def get_collection_class_from_document_meta_class(cls) -> Optional[Type]:
     if document_meta is not None:
         warnings.warn(
             "Class DocumentMeta is deprecated. "
-            "It will not be after 0.4.0. "
-            "Please use class Collection instead.",
+            "It will not be supported after 0.4.0. "
+            "Please use class Collection instead. "
+            "https://roman-right.github.io/beanie/#collection-setup",
             category=DeprecationWarning,
         )
         collection_name = getattr(document_meta, "collection_name", None)
