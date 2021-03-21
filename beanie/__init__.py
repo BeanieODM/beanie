8de672ccf23fbdcce1ecadbcdb6c2eeba388a26c
Roman <roman-right@protonmail.com>
2021-03-21 21:12:41 +0100

    version in the init file

--- beanie/__init__.py
+++ beanie/__init__.py
@@ -2,5 +2,5 @@ from beanie.cursor import Cursor
 from beanie.documents import Document
 from beanie.general import init_beanie
 
-__version__ = "0.3.0"
+__version__ = "0.3.1"
 __all__ = ["Document", "Cursor", "init_beanie"]
