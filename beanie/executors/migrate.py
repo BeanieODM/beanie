de4e3ec63591f4e7f4e23ec64fe48a600e91835e
Roman <roman-right@protonmail.com>
2021-04-02 22:23:06 +0200

    migrations toml settings support

--- beanie/executors/migrate.py
+++ beanie/executors/migrate.py
@@ -2,8 +2,10 @@ import asyncio
 import shutil
 from datetime import datetime
 from pathlib import Path
+from typing import Dict, Any
 
 import click
+import toml
 from pydantic import BaseSettings
 
 from beanie.migrations import template
@@ -12,12 +14,53 @@ from beanie.migrations.models import RunningMode, RunningDirections
 from beanie.migrations.runner import MigrationNode
 
 
+def toml_config_settings_source(settings: BaseSettings) -> Dict[str, Any]:
+    path = Path("pyproject.toml")
+    if path.is_file():
+        return (
+            toml.load(path)
+            .get("tool", {})
+            .get("beanie", {})
+            .get("migrations", {})
+        )
+    return {}
+
+
 class MigrationSettings(BaseSettings):
     direction: RunningDirections = RunningDirections.FORWARD
     distance: int = 0
     uri: str
     db: str
     path: Path
+
+    class Config:
+        env_prefix = "beanie_"
+        fields = {
+            "uri": {
+                "env": [
+                    "uri",
+                    "connection_string",
+                    "mongodb_dsn",
+                    "mongodb_url",
+                    "mongodb_uri",
+                ]
+            },
+            "db": {"env": ["db", "db_name", "database_name"]},
+        }
+
+        @classmethod
+        def customise_sources(
+            cls,
+            init_settings,
+            env_settings,
+            file_secret_settings,
+        ):
+            return (
+                init_settings,
+                toml_config_settings_source,
+                env_settings,
+                file_secret_settings,
+            )
 
 
 @click.group()
@@ -38,37 +81,45 @@ async def run_migrate(settings: MigrationSettings):
 @click.option(
     "--forward",
     "direction",
-    default=True,
+    required=False,
     flag_value="FORWARD",
     help="Direction of the migration",
-    show_default=True,
 )
 @click.option(
     "--backward",
     "direction",
+    required=False,
     flag_value="BACKWARD",
     help="Direction of the migration",
 )
 @click.option(
     "-d",
     "--distance",
-    default=0,
+    required=False,
     help="How many migrations should be done",
-    show_default=True,
 )
-@click.option("--uri", required=True, type=str, help="MongoDB connection URI")
-@click.option("--db", required=True, type=str, help="DataBase name")
+@click.option("--uri", required=False, type=str, help="MongoDB connection URI")
+@click.option("--db", required=False, type=str, help="DataBase name")
 @click.option(
     "-p",
     "--path",
-    required=True,
+    required=False,
     type=str,
     help="Path to the migrations directory",
 )
 def migrate(direction, distance, uri, db, path):
-    settings = MigrationSettings(
-        direction=direction, distance=distance, uri=uri, db=db, path=path
-    )
+    settings_kwargs = {}
+    if direction:
+        settings_kwargs["direction"] = direction
+    if distance:
+        settings_kwargs["distance"] = distance
+    if uri:
+        settings_kwargs["uri"] = uri
+    if db:
+        settings_kwargs["db"] = db
+    if path:
+        settings_kwargs["path"] = path
+    settings = MigrationSettings(**settings_kwargs)
     asyncio.run(run_migrate(settings))
 
 
