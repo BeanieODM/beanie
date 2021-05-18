5e2cc8144658ba311f27679a9ccd7fb2990c84d0
Roman <roman-right@protonmail.com>
2021-05-18 15:42:13 +0200

    version update

--- beanie/__init__.py
+++ beanie/__init__.py
@@ -4,7 +4,7 @@ from beanie.odm.fields import PydanticObjectId, Indexed
 from beanie.odm.utils.general import init_beanie
 from beanie.odm.documents import Document
 
-__version__ = "1.0.3"
+__version__ = "1.0.4"
 __all__ = [
     # ODM
     "Document",
