a628404a7a3383a201a87b0c97fccf27b297bddf
Roman <roman-right@protonmail.com>
2021-07-08 10:25:45 +0200

    fix: doc generation

--- scripts/publish_docs.sh
+++ scripts/publish_docs.sh
@@ -1,3 +1,8 @@
 poetry run pydoc-markdown
 cd docs/build
+
+remote_repo="https://x-access-token:${GITHUB_TOKEN}@${GITHUB_DOMAIN:-"github.com"}/${GITHUB_REPOSITORY}.git"
+git remote rm origin
+git remote add origin "${remote_repo}"
+
 poetry run mkdocs gh-deploy
