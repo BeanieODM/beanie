f87beb3ff07ccbb5a70508efffa5cdab90299dc2
Roman Right <roman-right@protonmail.com>
2021-12-08 17:31:40 +0100

    reworked

--- beanie/odm/actions.py
+++ beanie/odm/actions.py
@@ -4,12 +4,6 @@ from enum import Enum
 from functools import wraps
 from typing import Callable, List, Union, Dict, TYPE_CHECKING, Any, Type
 
-from beanie.odm.utils.class_path import (
-    get_class_for_method,
-    get_class_for_object,
-)
-from beanie.odm.utils.general import get_model
-
 if TYPE_CHECKING:
     from beanie.odm.documents import Document
 
@@ -33,29 +27,34 @@ class ActionDirections(str, Enum):  # TODO think about this name
 
 
 class ActionRegistry:
-    _actions: Dict[str, Any] = {}
-    _classes: Dict[str, Type] = {}
+    _actions: Dict[Type, Any] = {}
 
     # TODO the real type is
     #  Dict[str, Dict[EventTypes,Dict[ActionDirections: List[Callable]]]]
     #  But mypy says it has syntax error inside. Fix it.
 
     @classmethod
+    def clean_actions(cls, document_class: Type):
+        if not cls._actions.get(document_class) is None:
+            del cls._actions[document_class]
+
+    @classmethod
     def add_action(
         cls,
+        document_class: Type,
         event_types: List[EventTypes],
         action_direction: ActionDirections,
         funct: Callable,
     ):
         """
         Add action to the action registry
+        :param document_class: document class
         :param event_types: List[EventTypes]
         :param action_direction: ActionDirections - before or after
         :param funct: Callable - function
         """
-        class_instance = get_class_for_method(funct)
-        if cls._actions.get(class_instance) is None:
-            cls._actions[class_instance] = {
+        if cls._actions.get(document_class) is None:
+            cls._actions[document_class] = {
                 action_type: {
                     action_direction: []
                     for action_direction in ActionDirections
@@ -63,35 +62,27 @@ class ActionRegistry:
                 for action_type in EventTypes
             }
         for event_type in event_types:
-            cls._actions[class_instance][event_type][action_direction].append(
+            cls._actions[document_class][event_type][action_direction].append(
                 funct
             )
 
     @classmethod
     def get_action_list(
         cls,
-        class_path: Type,
-        event_types: EventTypes,
+        document_class: Type,
+        event_type: EventTypes,
         action_direction: ActionDirections,
     ) -> List[Callable]:
         """
         Get stored action list
-        :param class_path: str - path to the class
-        :param event_types: EventTypes - type of needed event
+        :param document_class: Type - document class
+        :param event_type: EventTypes - type of needed event
         :param action_direction: ActionDirections - before or after
         :return: List[Callable] - list of stored methods
         """
-        for registered_class_path in cls._actions:
-            if registered_class_path not in cls._classes:
-                cls._classes[registered_class_path] = get_model(
-                    registered_class_path
-                )
-            registered_class = cls._classes[registered_class_path]
-            if issubclass(class_path, registered_class):
-                return cls._actions[registered_class_path][event_types][
-                    action_direction
-                ]
-        return []
+        if document_class not in cls._actions:
+            return []
+        return cls._actions[document_class][event_type][action_direction]
 
     @classmethod
     async def run_actions(
@@ -106,9 +97,9 @@ class ActionRegistry:
         :param event_type: EventTypes - event types
         :param action_direction: ActionDirections - before or after
         """
-        class_path = get_class_for_object(instance)
+        document_class = instance.__class__
         actions_list = cls.get_action_list(
-            class_path, event_type, action_direction
+            document_class, event_type, action_direction
         )
         coros = []
         for action in actions_list:
@@ -134,11 +125,9 @@ def register_action(
         event_types = [event_types]
 
     def decorator(f):
-        ActionRegistry.add_action(
-            event_types=event_types,  # type: ignore
-            action_direction=action_direction,
-            funct=f,
-        )
+        f.has_action = True
+        f.event_types = event_types
+        f.action_direction = action_direction
         return f
 
     return decorator
--- beanie/odm/utils/class_path.py
+++
@@ -1,9 +0,0 @@
-from typing import Callable, Any, Type
-
-
-def get_class_for_method(f: Callable) -> str:
-    return f"{f.__module__}.{f.__qualname__.split('.')[0]}"
-
-
-def get_class_for_object(o: Any) -> Type:
-    return o.__class__
