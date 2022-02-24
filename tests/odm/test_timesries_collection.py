33cfd3cc661e1d713b06ac6aa184731e111620c1
Roman Right <roman-right@protonmail.com>
2022-02-24 16:43:41 +0100

    check mongodb version in test

--- tests/odm/test_timesries_collection.py
+++ tests/odm/test_timesries_collection.py
@@ -1,18 +1,26 @@
 async def test_timeseries_collection(db):
-    info = await db.command(
-        {"listCollections": 1, "filter": {"name": "DocumentWithTimeseries"}}
-    )
+    build_info = await db.command({"buildInfo": 1})
+    mongo_version = build_info["version"]
+    major_version = int(mongo_version.split(".")[0])
 
-    assert info["cursor"]["firstBatch"][0] == {
-        "name": "DocumentWithTimeseries",
-        "type": "timeseries",
-        "options": {
-            "expireAfterSeconds": 2,
-            "timeseries": {
-                "timeField": "ts",
-                "granularity": "seconds",
-                "bucketMaxSpanSeconds": 3600,
+    if major_version >= 5:
+        info = await db.command(
+            {
+                "listCollections": 1,
+                "filter": {"name": "DocumentWithTimeseries"},
+            }
+        )
+
+        assert info["cursor"]["firstBatch"][0] == {
+            "name": "DocumentWithTimeseries",
+            "type": "timeseries",
+            "options": {
+                "expireAfterSeconds": 2,
+                "timeseries": {
+                    "timeField": "ts",
+                    "granularity": "seconds",
+                    "bucketMaxSpanSeconds": 3600,
+                },
             },
-        },
-        "info": {"readOnly": False},
-    }
+            "info": {"readOnly": False},
+        }
