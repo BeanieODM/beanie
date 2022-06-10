e04e549effe3f4e5ca8b3efd0e5a6e85eb5a9ad4
Roman Right <roman-right@protonmail.com>
2022-06-10 16:40:14 +0200

    motor3 support

--- beanie/__init__.py
+++ beanie/__init__.py
@@ -25,7 +25,7 @@ from beanie.odm.documents import Document
 from beanie.odm.views import View
 from beanie.odm.union_doc import UnionDoc
 
-__version__ = "1.11.2"
+__version__ = "1.11.3"
 __all__ = [
     # ODM
     "Document",
