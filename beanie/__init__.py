bbea0d731ffe95bbb18b85a6351568727234e71d
Roman <roman-right@protonmail.com>
2022-09-26 16:49:37 +0200

    fix: remove prints

--- beanie/__init__.py
+++ beanie/__init__.py
@@ -26,7 +26,7 @@ from beanie.odm.documents import Document
 from beanie.odm.views import View
 from beanie.odm.union_doc import UnionDoc
 
-__version__ = "1.11.10"
+__version__ = "1.11.11"
 __all__ = [
     # ODM
     "Document",
