4ac356f753e78a6b9e212ac4a48dd21fcf13e91f
Roman <roman-right@protonmail.com>
2022-10-06 17:21:47 +0200

    version: 1.12.0

--- beanie/__init__.py
+++ beanie/__init__.py
@@ -26,7 +26,7 @@ from beanie.odm.documents import Document
 from beanie.odm.views import View
 from beanie.odm.union_doc import UnionDoc
 
-__version__ = "1.11.12"
+__version__ = "1.12.0"
 __all__ = [
     # ODM
     "Document",
