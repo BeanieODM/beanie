9a20f3225116e06e4e3a29688701083ad3912c80
vitalium <vitaliy@xserver.ua>
2022-10-25 15:34:19 +0300

    Simplified projection for inherited classes

--- beanie/odm/utils/projection.py
+++ beanie/odm/utils/projection.py
@@ -16,26 +16,19 @@ def get_projection(
     ):
         return None
 
-    if getattr(model.Config, "extra", None) == "allow":
-        return None
-
-    document_projection: Dict[str, int] = {}
-
     if hasattr(model, "Settings"):  # MyPy checks
         settings = getattr(model, "Settings")
+
         if hasattr(settings, "projection"):
             return getattr(settings, "projection")
 
         if getattr(settings, 'single_root_inheritance', False):
-            # include class_id if model has children or is child of some parent
-            if model.is_part_of_inheritance():
-                document_projection['_class_id'] = 1
+            return None
 
-            # include also all possible fields of any children
-            for child in model.get_children():
-                for name, field in child.__fields__.items():
-                    if field.alias not in document_projection:
-                        document_projection[field.alias] = 1
+    if getattr(model.Config, "extra", None) == "allow":
+        return None
+
+    document_projection: Dict[str, int] = {}
 
     for name, field in model.__fields__.items():
         document_projection[field.alias] = 1
