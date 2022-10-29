9ef329d46635498115dcfaab7cb76de408415279
Roman <roman-right@protonmail.com>
2022-10-29 22:01:41 +0200

    init controller started 5

--- beanie/odm/interfaces/inheritance.py
+++ beanie/odm/interfaces/inheritance.py
@@ -1,20 +1,9 @@
 from typing import (
     Type,
     Optional,
-    TYPE_CHECKING,
     Dict,
     ClassVar,
 )
-
-from pydantic import BaseModel
-
-if TYPE_CHECKING:
-    pass
-
-
-class Output(BaseModel):
-    class_name: str
-    collection_name: str
 
 
 class InheritanceInterface:
