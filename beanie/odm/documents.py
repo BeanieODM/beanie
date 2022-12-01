9ed52504763d0864a9d37240d6c2c2ccdcdcc6d3
Roman Right <roman-right@protonmail.com>
2022-12-01 11:28:03 +0100

    feat: lazy_parsing b2

--- beanie/odm/documents.py
+++ beanie/odm/documents.py
@@ -129,10 +129,6 @@ class Document(
     # Other
     _hidden_fields: ClassVar[Set[str]] = set()
 
-    # Lazy getter
-    _store: Dict[str, Any] = PrivateAttr(default_factory=dict)
-    _lazily_parsed: bool = PrivateAttr(default=False)
-
     def _swap_revision(self):
         if self.get_settings().use_revision:
             self._previous_revision_id = self.revision_id
@@ -941,26 +937,6 @@ class Document(
     def link_from_id(cls, id: Any):
         ref = DBRef(id=id, collection=cls.get_collection_name())
         return Link(ref, model_class=cls)
-
-    # Lazy getters
-
-    def set_store(self, d):
-        self._store = d
-
-    def __getattribute__(self, item):
-        res = super(Document, self).__getattribute__(item)
-        if isinstance(res, ExpressionField):
-            field_info = self.__fields__.get(item)
-            value = self._store[field_info.alias]
-            self.__setattr__(item, value)
-            return super(Document, self).__getattribute__(item)
-        return res
-
-    def fetch_from_store(self):
-        if self._lazily_parsed:
-            for k, v in self.__fields__.items():
-                self.__getattribute__(v.name)
-            self._lazily_parsed = False
 
     class Config:
         json_encoders = {
