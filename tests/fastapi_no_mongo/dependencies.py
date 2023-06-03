9947d041aa62a979daa4df8cc745dc521389fc5f
Yuval Levi <30751157+yuval9313@users.noreply.github.com>
2023-06-04 00:34:29 +0300

    Update dependencies.py
    Add explanation about function with pass in it

--- tests/fastapi_no_mongo/dependencies.py
+++ tests/fastapi_no_mongo/dependencies.py
@@ -1,6 +1,8 @@
+# Dependency declared here so it could be overridden
 def provide_windows_repository():
     pass
 
 
+# Dependency declared here so it could be overridden
 def provide_house_repository():
     pass
