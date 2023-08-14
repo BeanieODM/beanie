5ac75e62eb1cfff8c2cfa62617c5fe25c16a4b64
George Sakkis <george.sakkis@gmail.com>
2023-08-14 08:18:52 +0300

    Relax extract_id_class to support arbitrary generic types

--- tests/odm/test_typing_utils.py
+++ tests/odm/test_typing_utils.py
@@ -1,6 +1,11 @@
 from typing import Optional, Union
 
 from beanie.odm.utils.typing import extract_id_class
+from beanie import Document, Link
+
+
+class Lock(Document):
+    k: int
 
 
 class TestTyping:
@@ -11,3 +16,5 @@ class TestTyping:
         assert extract_id_class(Union[str, None, int]) == str
         # Optional
         assert extract_id_class(Optional[str]) == str
+        # Link
+        assert extract_id_class(Link[Lock]) == Lock
