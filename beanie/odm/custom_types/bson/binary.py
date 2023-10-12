8851a510d8539c9d8c3f06e020a255eae4d41add
George Sakkis <george.sakkis@gmail.com>
2023-10-12 03:55:22 +0300

    Simplify BsonBinary

--- beanie/odm/custom_types/bson/binary.py
+++ beanie/odm/custom_types/bson/binary.py
@@ -1,65 +1,23 @@
-from typing import Any, Callable
+from typing import Any
 
 import bson
+import pydantic
+from typing_extensions import Annotated
 
 from beanie.odm.utils.pydantic import IS_PYDANTIC_V2
 
+
+def _to_bson_binary(value: Any) -> bson.Binary:
+    return value if isinstance(value, bson.Binary) else bson.Binary(value)
+
+
 if IS_PYDANTIC_V2:
-    from pydantic import GetJsonSchemaHandler
-    from pydantic.fields import FieldInfo
-    from pydantic.json_schema import JsonSchemaValue
-    from pydantic_core import core_schema
+    BsonBinary = Annotated[
+        bson.Binary, pydantic.PlainValidator(_to_bson_binary)
+    ]
+else:
 
-
-class BsonBinary(bson.Binary):
-    if IS_PYDANTIC_V2:
-
-        @classmethod
-        def __get_pydantic_core_schema__(
-            cls,
-            _source_type: Any,
-            _handler: Callable[[Any], core_schema.CoreSchema],  # type: ignore
-        ) -> core_schema.CoreSchema:  # type: ignore
-            def validate(value, _: FieldInfo) -> bson.Binary:
-                if isinstance(value, BsonBinary):
-                    return value
-                if isinstance(value, bson.Binary):
-                    return BsonBinary(value)
-                if isinstance(value, bytes):
-                    return BsonBinary(value)
-                raise ValueError(
-                    "Value must be bytes or bson.Binary or BsonBinary"
-                )
-
-            python_schema = core_schema.general_plain_validator_function(validate)  # type: ignore
-
-            return core_schema.json_or_python_schema(
-                json_schema=core_schema.float_schema(),
-                python_schema=python_schema,
-            )
-
-        @classmethod
-        def __get_pydantic_json_schema__(
-            cls,
-            _core_schema: core_schema.CoreSchema,  # type: ignore
-            handler: GetJsonSchemaHandler,
-        ) -> JsonSchemaValue:
-            return handler(core_schema.str_schema())
-
-    else:
-
+    class BsonBinary(bson.Binary):  # type: ignore[no-redef]
         @classmethod
         def __get_validators__(cls):
-            yield cls.validate
-
-        @classmethod
-        def validate(cls, value):
-            if isinstance(value, BsonBinary):
-                return value
-            if isinstance(value, bson.Binary):
-                return BsonBinary(value)
-            if isinstance(value, bytes):
-                return BsonBinary(value)
-            raise ValueError(
-                "Value must be bytes or bson.Binary or BsonBinary"
-            )
+            yield _to_bson_binary
--- tests/odm/custom_types/test_bson_binary.py
+++ tests/odm/custom_types/test_bson_binary.py
@@ -1,20 +1,24 @@
 import bson
+import pytest
 
 from beanie import BsonBinary
+from beanie.odm.utils.pydantic import get_model_dump, parse_model
 from tests.odm.models import DocumentWithBsonBinaryField
 
 
-async def test_bson_binary():
-    doc = DocumentWithBsonBinaryField(binary_field=bson.Binary(b"test"))
+@pytest.mark.parametrize("binary_field", [bson.Binary(b"test"), b"test"])
+async def test_bson_binary(binary_field):
+    doc = DocumentWithBsonBinaryField(binary_field=binary_field)
     await doc.insert()
     assert doc.binary_field == BsonBinary(b"test")
 
     new_doc = await DocumentWithBsonBinaryField.get(doc.id)
     assert new_doc.binary_field == BsonBinary(b"test")
 
-    doc = DocumentWithBsonBinaryField(binary_field=b"test")
-    await doc.insert()
-    assert doc.binary_field == BsonBinary(b"test")
 
-    new_doc = await DocumentWithBsonBinaryField.get(doc.id)
-    assert new_doc.binary_field == BsonBinary(b"test")
+@pytest.mark.parametrize("binary_field", [bson.Binary(b"test"), b"test"])
+def test_bson_binary_roundtrip(binary_field):
+    doc = DocumentWithBsonBinaryField(binary_field=binary_field)
+    doc_dict = get_model_dump(doc)
+    new_doc = parse_model(DocumentWithBsonBinaryField, doc_dict)
+    assert new_doc == doc
