31b76bbc25736a099ef4414e510cda0c089a7c05
Roman <roman-right@protonmail.com>
2023-11-12 17:01:58 -0600

    args for get_model_dump

--- beanie/__init__.py
+++ beanie/__init__.py
@@ -17,6 +17,7 @@ from beanie.odm.bulk import BulkWriter
 from beanie.odm.custom_types import DecimalAnnotation
 from beanie.odm.custom_types.bson.binary import BsonBinary
 from beanie.odm.documents import Document
+from beanie.odm.enums import SortDirection
 from beanie.odm.fields import (
     BackLink,
     DeleteRules,
@@ -42,6 +43,7 @@ __all__ = [
     "Indexed",
     "TimeSeriesConfig",
     "Granularity",
+    "SortDirection",
     # Actions
     "before_event",
     "after_event",
