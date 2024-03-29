1e8d1c5d9563e9a1faee6bbd56018c7e0bb44ecd
CAPITAINMARVEL <91954476+CAPITAINMARVEL@users.noreply.github.com>
2024-03-29 16:48:25 +0100

    added delete_all with children + test from https://github.com/roman-right/beanie/pull/866

--- beanie/odm/documents.py
+++ beanie/odm/documents.py
@@ -890,6 +890,7 @@ class Document(
         cls,
         session: Optional[ClientSession] = None,
         bulk_writer: Optional[BulkWriter] = None,
+        with_children: bool = False,
         **pymongo_kwargs,
     ) -> Optional[DeleteResult]:
         """
@@ -899,7 +900,7 @@ class Document(
         :param **pymongo_kwargs: pymongo native parameters for delete operation
         :return: Optional[DeleteResult] - pymongo DeleteResult instance.
         """
-        return await cls.find_all().delete(
+        return await cls.find_all(with_children=with_children).delete(
             session=session, bulk_writer=bulk_writer, **pymongo_kwargs
         )
 
