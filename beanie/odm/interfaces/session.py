8644540238aff8407349fadf4e449437ed4e6955
Batista Harahap <batista@bango29.com>
2024-05-23 22:24:25 +0400

    adding documents with session

--- beanie/odm/interfaces/session.py
+++ beanie/odm/interfaces/session.py
@@ -1,6 +1,8 @@
-from typing import Optional
+from contextvars import ContextVar
+from typing import Optional, ClassVar
 
 from pymongo.client_session import ClientSession
+import motor.motor_asyncio
 
 
 class SessionMethods:
@@ -17,3 +19,39 @@ class SessionMethods:
         if session is not None:
             self.session: Optional[ClientSession] = session
         return self
+
+
+class DocumentSessionMethods:
+    """
+    Document session methods
+    """
+
+    __session__: ClassVar[ContextVar[Optional[motor.motor_asyncio.AsyncIOMotorClientSession]]] = ContextVar(
+        "session", default=None
+    )
+
+    @classmethod
+    def set_session(cls, session: Optional[motor.motor_asyncio.AsyncIOMotorClientSession] = None):
+        """
+        Set session to the ClassVar context
+        :param session: Optional[ClientSession] - pymongo session
+        :return:
+        """
+        if session is not None:
+            cls.__session__.set(session)
+
+    @classmethod
+    def get_session(cls) -> Optional[motor.motor_asyncio.AsyncIOMotorClientSession]:
+        """
+        Get session from the ClassVar context
+        :return: Optional[ClientSession]
+        """
+        return cls.__session__.get()
+
+    @classmethod
+    def clear_session(cls):
+        """
+        Clear session from the ClassVar context
+        :return:
+        """
+        cls.__session__.set(None)
