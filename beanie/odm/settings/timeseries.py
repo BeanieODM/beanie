c773c136f1d50c764a30f2ac3127287f4fbd047a
m00rex <48015628+m00rex@users.noreply.github.com>
2025-04-28 16:32:57 +0200

    Update timeseries.py

--- beanie/odm/settings/timeseries.py
+++ beanie/odm/settings/timeseries.py
@@ -1,7 +1,7 @@
 from enum import Enum
 from typing import Any, Dict, Optional
 
-from pydantic import Annotated, BaseModel
+from pydantic import Annotated, BaseModel, Field
 
 
 class Granularity(str, Enum):
