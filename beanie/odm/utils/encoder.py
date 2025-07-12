f3c74de0a6029b0affd993f431a1e10640b90b64
mg3146 <markgillis0@gmail.com>
2025-07-12 19:59:58 -0400

    Update encoder.py
    fixed the __iter__() call.

--- beanie/odm/utils/encoder.py
+++ beanie/odm/utils/encoder.py
@@ -151,27 +151,27 @@ class Encoder:
 
         raise ValueError(f"Cannot encode {obj!r}")
 
-    def _iter_model_items(
-        self, obj: pydantic.BaseModel
-    ) -> Iterable[Tuple[str, Any]]:
+    def _iter_model_items(self, obj: pydantic.BaseModel) -> Iterable[Tuple[str, Any]]:
         exclude, keep_nulls = self.exclude, self.keep_nulls
         get_model_field = get_model_fields(obj).get
 
-        obj_iter_keys = obj.__iter__.keys()
         if IS_PYDANTIC_V2:
-            if obj.__class__.model_config.get("extra") != "allow":
-                model_class = obj.__class__
-                obj_iter_keys = list(model_class.model_fields.keys()) + list(
-                    model_class.model_computed_fields.keys()
-                )
-
+            model_class = obj.__class__``
+            if model_class.model_config.get("extra") != "allow":
+                obj_iter_keys = set(model_class.model_fields.keys()) | set(
+                model_class.model_computed_fields.keys()
+            )
+            else:
+                obj_iter_keys = {k for k, _ in obj.__iter__()}
+        else:
+            obj_iter_keys = {k for k, _ in obj.__iter__()}
+    
         for key, value in obj.__iter__():
             if key in obj_iter_keys:
                 field_info = get_model_field(key)
-                if field_info is not None:
-                    key = field_info.alias or key
-                if key not in exclude and (value is not None or keep_nulls):
-                    yield key, value
+                out_key = field_info.alias or key if field_info else key
+                if out_key not in exclude and (value is not None or keep_nulls):
+                    yield out_key, value
 
 
 def _get_encoder(
