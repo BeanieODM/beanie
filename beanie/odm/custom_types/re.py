300ee5fac03fab163ab23980bdf28ee9b17eaac8
ᑕᗩᑭTᗩIᑎᗰᗩᖇᐯᗴᒪ <91954476+CAPITAINMARVEL@users.noreply.github.com>
2026-02-20 21:00:03 +0100

    Remove unused _to_bson_regex function
    Removed the unused _to_bson_regex function from re.py.

--- beanie/odm/custom_types/re.py
+++ beanie/odm/custom_types/re.py
@@ -5,10 +5,6 @@ import pydantic
 from typing_extensions import Annotated
 
 
-def _to_bson_regex(v):
-    return v.try_compile() if isinstance(v, bson.Regex) else v
-
-
 Pattern = Annotated[
     re.Pattern,
     pydantic.BeforeValidator(
