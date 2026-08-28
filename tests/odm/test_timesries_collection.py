480495fe2defcda940253ffd807a83b0e4964b31
mehdi.sam <@/2212rtgVWQqQ>
2026-08-28 19:53:55 +0330

    Fix polymorphic timeseries collection initialization

--- tests/odm/test_timesries_collection.py
+++ tests/odm/test_timesries_collection.py
@@ -2,7 +2,12 @@ import pytest
 
 from beanie import init_beanie
 from beanie.exceptions import MongoDBVersionError
-from tests.odm.models import DocumentWithTimeseries
+from tests.odm.models import (
+    DocumentWithTimeseries,
+    HumidityMeasurement,
+    MeasurementWithTimeseries,
+    TemperatureMeasurement,
+)
 
 
 async def test_timeseries_collection(db):
@@ -31,6 +36,51 @@ async def test_timeseries_collection(db):
             "type": "timeseries",
             "options": {
                 "expireAfterSeconds": 2,
+                "timeseries": {
+                    "timeField": "ts",
+                    "granularity": "seconds",
+                    "bucketMaxSpanSeconds": 3600,
+                },
+            },
+            "info": {"readOnly": False},
+        }
+
+
+async def test_timeseries_polymorphic_collection(db):
+    build_info = await db.command({"buildInfo": 1})
+    mongo_version = build_info["version"]
+    major_version = int(mongo_version.split(".")[0])
+    if major_version < 5:
+        with pytest.raises(MongoDBVersionError):
+            await init_beanie(
+                database=db,
+                document_models=[
+                    MeasurementWithTimeseries,
+                    TemperatureMeasurement,
+                    HumidityMeasurement,
+                ],
+            )
+
+    if major_version >= 5:
+        await db.drop_collection("measurements")
+        await init_beanie(
+            database=db,
+            document_models=[
+                MeasurementWithTimeseries,
+                TemperatureMeasurement,
+                HumidityMeasurement,
+            ],
+        )
+        info = await db.command(
+            {
+                "listCollections": 1,
+                "filter": {"name": "measurements"},
+            }
+        )
+        assert info["cursor"]["firstBatch"][0] == {
+            "name": "measurements",
+            "type": "timeseries",
+            "options": {
                 "timeseries": {
                     "timeField": "ts",
                     "granularity": "seconds",
